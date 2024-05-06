55a5e340647f7a7027ad03b811365455d3a526d6
arshassalim7696 <167634160+arshassalim7696@users.noreply.github.com>
2024-05-06 08:23:56 +0530

    testcommit

---
+++ ConstructorSamppple.java
@@ -0,0 +1,20 @@
+class ConstructorSamppple
+{
+char c;
+float b;
+public ConstructorSamppple(char c,float b)
+{
+this.c=c;
+this.b=b;
+System.out.println(c);
+System.out.println(b);
+
+
+}
+public static void main(String args[])
+{
+	ConstructorSamppple obj=new ConstructorSamppple('z',4.7f);
+}
+}
+
+	
